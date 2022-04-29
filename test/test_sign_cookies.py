920d598bd252fe77f2134ee97b4b9d89b2811005
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-04-29 16:09:46 +0100

    Add test AWS credentials

--- test/test_sign_cookies.py
+++ test/test_sign_cookies.py
@@ -59,6 +59,11 @@ class TestStringMethods(TestCase):
         os.environ["AWS_LAMBDA_FUNCTION_NAME"] = "test-function-name"
         os.environ["KEY_PAIR_ID"] = encrypt(kms_key, kms, "key-pair-id")
         os.environ["PRIVATE_KEY"] = encrypt(kms_key, kms, private_key())
+        os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+        os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+        os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+        os.environ['AWS_SESSION_TOKEN'] = 'testing'
+        os.environ['AWS_DEFAULT_REGION'] = 'eu-west-2'
 
     def test_unauthorised_for_invalid_token(self):
         event = {
