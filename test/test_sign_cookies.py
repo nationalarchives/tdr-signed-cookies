e4aaf481b29f3aa9d68c1ddbfda7d8422beeff9c
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-04-29 16:11:04 +0100

    Use region instead of default region

--- test/test_sign_cookies.py
+++ test/test_sign_cookies.py
@@ -63,7 +63,7 @@ class TestStringMethods(TestCase):
         os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
         os.environ['AWS_SECURITY_TOKEN'] = 'testing'
         os.environ['AWS_SESSION_TOKEN'] = 'testing'
-        os.environ['AWS_DEFAULT_REGION'] = 'eu-west-2'
+        os.environ['AWS_REGION'] = 'eu-west-2'
 
     def test_unauthorised_for_invalid_token(self):
         event = {
