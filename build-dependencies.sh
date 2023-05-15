2402311e2002adcc0cedfc836269d25912753326
vimleshg <vimlesh.gupta@nationalarchives.gov.uk>
2023-05-15 12:11:33 +0100

    Trying to fix the issue

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -5,14 +5,14 @@ python3.9 -m ensurepip --upgrade
 
 mkdir /pip
 pip3.9 install --requirement requirements.txt --target /pip
-pip3.9 install --platform manylinux2010_x86_64 --implementation cp --python 3.9 --only-binary=:all: --upgrade --target awsbundle cryptography
+pip3.9 install --platform manylinux2010_x86_64 --implementation cp --python 3.9 --only-binary=:all: --upgrade --target /pip cryptography
 
 cd /pip
 rm -r *.dist-info
 find . -name __pycache__ | xargs rm -r
-ls awsbundle/
-mv awsbundle/_cffi_backend.cpython-36m-x86_64-linux-gnu.so _cffi_backend.so
-cd awsbundle/cryptography/hazmat/bindings
+ls
+mv _cffi_backend.cpython-36m-x86_64-linux-gnu.so _cffi_backend.so
+cd cryptography/hazmat/bindings
 mv _openssl.abi3.so _openssl.so
 
 mkdir /lambda
