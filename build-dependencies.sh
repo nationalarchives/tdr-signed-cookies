ee35551bc88a290ee3766a95c71ed2b7ac095922
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2025-08-13 16:24:28 +0100

    Update library name

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -7,13 +7,13 @@ mkdir /pip
 python3.11 -m pip install --upgrade pip
 pip3.11 install --requirement requirements.txt --target /pip
 python3.11 -m pip install setuptools_rust
-pip3.11 install --platform manylinux_2_12_x86_64 --implementation cp --python 3.11 --only-binary=:all: --upgrade --target /pip cryptography
+pip3.11 install --platform manylinux_2_12_x86_64 --implementation cp --only-binary=:all: --upgrade --target /pip cryptography
 
 cd /pip
 rm -r *.dist-info
 find . -name __pycache__ | xargs rm -r
 
-mv _cffi_backend.cpython-39-x86_64-linux-gnu.so _cffi_backend.so
+mv _cffi_backend.cpython-311-x86_64-linux-gnu.so _cffi_backend.so
 cd cryptography/hazmat/bindings
 mv _openssl.abi3.so _openssl.so
 
