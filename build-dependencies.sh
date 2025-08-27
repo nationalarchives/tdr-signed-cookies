32ba9017e1e42f474ed415b542b2cb285136b92e
VimleshGupta <vimlesh.gupta@nationalarchives.gov.uk>
2025-08-27 10:29:54 +0100

    Remove pinned version

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -6,15 +6,13 @@ python3.13 -m ensurepip --upgrade
 mkdir /pip
 python3.13 -m pip install --upgrade pip
 pip3.13 install --requirement requirements.txt --target /pip
-python3.13 -m pip install --platform manylinux2014_x86_64 --implementation cp --only-binary=:all: --upgrade --target /pip cryptography==43.0.1
+python3.13 -m pip install --platform manylinux2014_x86_64 --implementation cp --only-binary=:all: --upgrade --target /pip cryptography
 
 cd /pip
 rm -r *.dist-info
 find . -name __pycache__ | xargs rm -r
 
 mv _cffi_backend.cpython-313-x86_64-linux-gnu.so _cffi_backend.so
-cd cryptography/hazmat/bindings
-mv _openssl.abi3.so _openssl.so
 
 mkdir /lambda
 cp -r /pip/* /lambda
